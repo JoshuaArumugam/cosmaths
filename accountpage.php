8dfd2de074d07d484633af7d814d7d74800e9337
JoshuaArumugam <joshua.arumugam2009@gmail.com>
2026-05-07 09:45:38 +0100

    continued accountpage

--- accountpage.php
+++ accountpage.php
@@ -18,9 +18,39 @@
     </head>
     <body>
         <h1>Account Information</h1>
-        <p>Username</p>
-        <p>Email</p>
+        <p><b>Username:</b></p>
+        <?php 
+            // echo username in session variable
+            echo($_SESSION["username"]);
+        ?>
+        <p><b>Email:</b></p>
+        <?php 
+            // do same for email
+            echo($_SESSION["email"]);
+        ?>
         <h3>Interests</h3>
+        <form action="addinterest.php" method="post">
+            <label for="interesttoadd">Select interest to add:</label>
+            <select name="interesttoadd" id="interesttoadd">
+                <?php
+                    // connect to db and fetch all topics from table
+                    include_once('connection.php');
+
+                    $stmt = $conn->prepare("
+                    SELECT * FROM tbltopiclabels;
+                    ");
+                    $stmt->execute();
+
+                    // loop through returned records and add all options to the dropdown list
+
+                    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                        // create select option
+                        echo("<option value='" . $row["TopicID"] . "'>" . $row["TopicName"] . "</option>");
+                    }
+                ?>
+            </select>
+            <input type="submit" value="Add interest">
+        </form>
         <h3>Posts</h3>
     </body>
 </html>
