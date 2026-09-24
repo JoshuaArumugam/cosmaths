300c78cec352af8c35f33811e425c6e5291fea55
JoshuaArumugam <joshua.arumugam2009@gmail.com>
2026-09-24 10:40:18 +0100

    working on search

--- index.php
+++ index.php
@@ -49,8 +49,8 @@
             <input type="submit" value="Submit">
         </form>
         <?php
-            // check if madesearchquery is not 1
-            if ($_SESSION["madesearchquery"] != 1) {
+            // check if madesearchquery and madesearch are not 1
+            if ($_SESSION["madesearchquery"] != 1 && $_SESSION["madesearch"] != 1) {
                 // fetch all posts and sort by most likes
                 $stmt = $conn->prepare("
                 SELECT * FROM tblposts ORDER BY PostLikes DESC;
@@ -85,6 +85,40 @@
                     echo("<br>");
                     echo("</form>");
                 }
+            }
+            // check if madesearch is 1
+            elseif ($_SESSION["madesearch"] == 1) {
+                // loop through all returned posts and display all the post info
+                // add \\(\\) around latex so it gets rendered by mathjax
+                // everything is put inside a form element so postid can be posted to savepostid.php when user clicks anywhere on the post
+                while ($row = $_SESSION["searchresults"]->fetch(PDO::FETCH_ASSOC)) {
+                    // count how many comments on each post
+                    $stmt2 = $conn->prepare("
+                    SELECT COUNT(*) FROM tblcomments WHERE PostID = :PostID;
+                    ");
+
+                    // bind postid and execute
+                    $stmt2->bindParam(":PostID", $row["PostID"]);
+                    $stmt2->execute();
+
+                    // get comment count
+                    $row1 = $stmt2->fetch(PDO::FETCH_ASSOC);
+
+                    // echo all post info
+                    echo("<form action='savepostid.php' method='post' onclick='this.submit()' style='cursor: pointer;'>");
+                    echo("<input type='hidden' name='PostID' value='" . $row["PostID"] . "'>");
+                    echo("<h2>\\(" . $row["PostTitle"] . "\\)</h2>");
+                    echo("<p>\\(" . $row["PostContent"] . "\\)</p>");
+                    echo("<p><b>Likes:</b> " . $row["PostLikes"] . "</p>");
+                    echo("<p><b>Dislikes:</b> " . $row["PostDislikes"] . "</p>");
+                    echo("<p><b>Comments:</b> " . $row1["COUNT(*)"] . "</p>");
+                    echo("<p><b>Date:</b> " . $row["PostTime"] . "</p>");
+                    echo("<br>");
+                    echo("</form>");
+                }
+
+                // set madesearch back to 0
+                $_SESSION["madesearch"] = 0;
             }
             else {
                 // run sql statement and display all returned posts
--- processsearch.php
+++ processsearch.php
@@ -2,10 +2,26 @@
     // connect to db, start session, redirect back to index.php when done
     include_once('connection.php');
     session_start();
+
+    // set made search to 1
+    $_SESSION["madesearch"] = 1;
+    
     header("Location: index.php");
 
     // prevent sql injection
     array_map("htmlspecialchars", $_POST);
 
     // select all posts where search query is in post title, content or username
+    // make the fields lowercase so it doesn't matter if the text in the post is uppercase or not
+    $stmt = $conn->prepare("
+    SELECT * FROM tblposts WHERE LOWER(PostContent) LIKE CONCAT('%', :searchquery, '%')
+    OR LOWER(PostTitle) LIKE CONCAT('%', :searchquery, '%')
+    OR UserID IN (SELECT UserID FROM tblusers WHERE LOWER(Username) LIKE CONCAT('%', :searchquery, '%'));
+    ");
+    // bind params and execute
+    $stmt->bindParam(":searchquery", $_POST["searchquery"]);
+    $stmt->execute();
+
+    // store result of statement in session variable
+    $_SESSION["searchresults"] = $stmt;
 ?>
